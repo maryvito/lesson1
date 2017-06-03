ddab5d2378f36a98b04d98d099cb67a7456bbb9a
maryvito <maryv2@yandex.ru>
2017-06-03 12:56:59 +0300

    dz #1 answers

--- answers.py
+++ answers.py
@@ -2,6 +2,23 @@ def get_answer(question):
     answers = {'привет': 'И тебе привет!',
         'как дела': 'Лучше всех', 
         'пока': 'Увидимся', }
-    return answers[question.lower()]
+    return answers.get(question)
 
-print(get_answer('Как Дела'))
+def ask_user():
+    try:
+        while True:
+            user_input = input("Скажи что-нибудь! ")
+            print(get_answer(user_input))
+
+            if user_input == "Пока!":
+                print("Ну пока")
+                break
+    except KeyboardInterrupt:
+        print("Пока!")
+    
+
+
+
+
+if __name__ == "__main__":
+    ask_user()
